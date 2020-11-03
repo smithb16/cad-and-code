835693dc359f537914ba846a575771560fb70bfe
Ben Smith <benjamin.linden.smith@gmail.com>
2020-11-03 09:08:59 -0500

    Add contact form. Change font stack.

--- _site/process-contact-form.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $name = htmlspecialchars($_POST['user_name']);
-  $phone = htmlspecialchars($_POST['user_phone']);
-  $message  = htmlspecialchars($_POST['message']);
-
-  echo  $name,$phone,$message;
-?>
